05a93f56ffa1c7d18d060b2d15e091d9d1b6c364
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-10-05 15:43:01 +0800

    enhancement: added furniture list service

--- src/app/furniture-list/furniture-list.component.ts
+++ src/app/furniture-list/furniture-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import FurnitureList from '@app/interface/furniture-list';
+import { FurnitureListService } from '@app/services/furniture-list/furniture-list.service';
 
 @Component({
 	selector: 'app-furniture-list',
@@ -11,8 +12,10 @@ import FurnitureList from '@app/interface/furniture-list';
 	styles: ``,
 })
 export class FurnitureListComponent {
-	furnitureList: FurnitureList[] = [];
-
+	furnitureList = [] as FurnitureList[];
+	constructor(private furnitureService: FurnitureListService) {
+		this.furnitureList = this.furnitureService.getFurnitures();
+	}
 	name: string = '';
 	description: string = '';
 	type: string = '';
@@ -20,13 +23,13 @@ export class FurnitureListComponent {
 	category: string = '';
 
 	addFurniture() {
-		this.furnitureList.push({
-			name: this.name,
-			description: this.description,
-			type: this.type,
-			price: this.price,
-			category: this.category,
-		});
+		this.furnitureService.addFurniture(
+			this.name,
+			this.description,
+			this.type,
+			this.price,
+			this.category,
+		);
 		this.name = '';
 		this.description = '';
 		this.type = '';
---
+++ src/app/services/furniture-list/furniture-list.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import FurnitureList from '@app/interface/furniture-list';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class FurnitureListService {
+	private furnitures: FurnitureList[] = [];
+	constructor() {}
+
+	addFurniture(
+		name: string,
+		description: string,
+		type: string,
+		price: number,
+		category: string,
+	): void {
+		this.furnitures.push({
+			name: name,
+			description: description,
+			type: type,
+			price: price,
+			category: category,
+		});
+	}
+
+	getFurnitures(): FurnitureList[] {
+		return this.furnitures;
+	}
+}
