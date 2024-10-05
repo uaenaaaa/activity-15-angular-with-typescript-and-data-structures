6bf539612dfbdf983ba966226c35f7b17e3fa35f
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-10-05 16:06:54 +0800

    enhancement: added artist list service

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import ArtistList from '@app/interface/artist-list';
+import { ArtistListService } from '@app/services/artist-list/artist-list.service';
 
 @Component({
 	selector: 'app-artist-list',
@@ -11,7 +12,10 @@ import ArtistList from '@app/interface/artist-list';
 	styles: ``,
 })
 export class ArtistListComponent {
-	artistList: ArtistList[] = [];
+	artistList = [] as ArtistList[];
+	constructor(private artistService: ArtistListService) {
+		this.artistList = this.artistService.getArtists();
+	}
 
 	name: string = '';
 	field: string = '';
@@ -19,12 +23,12 @@ export class ArtistListComponent {
 	country: string = '';
 
 	addArtist() {
-		this.artistList.push({
-			name: this.name,
-			field: this.field,
-			genre: this.genre,
-			country: this.country,
-		});
+		this.artistService.addArtist(
+			this.name,
+			this.field,
+			this.genre,
+			this.country,
+		);
 		this.name = '';
 		this.field = '';
 		this.genre = '';
---
+++ src/app/services/artist-list/artist-list.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import ArtistList from '@app/interface/artist-list';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class ArtistListService {
+	private artists: ArtistList[] = [];
+
+	constructor() {}
+
+	addArtist(
+		name: string,
+		field: string,
+		genre: string,
+		country: string,
+	): void {
+		this.artists.push({
+			name: name,
+			field: field,
+			genre: genre,
+			country: country,
+		});
+	}
+
+	getArtists(): ArtistList[] {
+		return this.artists;
+	}
+}
