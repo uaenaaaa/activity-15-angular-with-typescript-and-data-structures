962acde4d2de8af1b79f067761f9cf2256680c93
froilanimnida <61849282+froilanimnida@users.noreply.github.com>
2024-10-05 10:19:49 +0800

    enhancement: added book list service

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import BookList from '@app/interface/book-list';
 import { FormsModule } from '@angular/forms';
 import { NgForOf } from '@angular/common';
+import { BookListService } from '@app/services/book-list/book-list.service';
 
 @Component({
 	selector: 'app-book-list',
@@ -11,19 +12,17 @@ import { NgForOf } from '@angular/common';
 	styles: ``,
 })
 export class BookListComponent {
-	books: BookList[] = [];
+	books = [] as BookList[];
+	constructor(private bookService: BookListService) {
+		this.books = this.bookService.getBooks();
+	}
 
 	id: string = '';
 	name: string = '';
 	isbn: string = '';
 
 	addBook(): void {
-		this.books.push({
-			id: this.id,
-			name: this.name,
-			isbn: this.isbn,
-		});
-
+		this.bookService.addBook(this.id, this.name, this.isbn);
 		this.id = '';
 		this.name = '';
 		this.isbn = '';
